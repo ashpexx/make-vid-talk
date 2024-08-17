eabdb652620423a9b04b4a649b1029364ca10b11
devsamahd <samahd626@gmail.com>
2024-08-17 23:47:32 +0100

    ...

--- app.py
+++ app.py
@@ -45,40 +45,43 @@ def server_active():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    if 'video' not in request.files or 'audio' not in request.files:
-        abort(400, 'Both video and audio files are required')
-    print("otilo bayen")
-    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    video_file = request.files['video']
-    audio_file = request.files['audio']
-
-    if not video_file or not audio_file:
-        abort(400, 'Both video and audio files are required')
-
-    video_slug = 'video'
-    audio_slug = 'audio'
-
-    # Upload files to DigitalOcean Spaces
-    video_url = do_service.upload_file(video_file.read(), video_slug, video_file.filename)
-    audio_url = do_service.upload_file(audio_file.read(), audio_slug, audio_file.filename)
-
-    # Validate file durations
     try:
-        do_service.validate_file_duration(video_url)
-        do_service.validate_file_duration(audio_url)
-    except ValueError as e:
-        abort(400, str(e))
+        if 'video' not in request.files or 'audio' not in request.files:
+            abort(400, 'Both video and audio files are required')
+        print("otilo bayen")
+        timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        video_file = request.files['video']
+        audio_file = request.files['audio']
 
-    # Process video and generate output
-    output_file = infer(video_url, audio_url)
-    thumbnail_url = do_service.generate_thumbnail(video_url, '/tmp', 'user-thumbnail')
+        if not video_file or not audio_file:
+            abort(400, 'Both video and audio files are required')
 
-    return jsonify({
-        'video_url': video_url,
-        'audio_url': audio_url,
-        'result_url': output_file,
-        'thumbnail_url': thumbnail_url
-    })
+        video_slug = 'video'
+        audio_slug = 'audio'
+
+        # Upload files to DigitalOcean Spaces
+        video_url = do_service.upload_file(video_file.read(), video_slug, video_file.filename)
+        audio_url = do_service.upload_file(audio_file.read(), audio_slug, audio_file.filename)
+
+        # Validate file durations
+        try:
+            do_service.validate_file_duration(video_url)
+            do_service.validate_file_duration(audio_url)
+        except ValueError as e:
+            abort(400, str(e))
+
+        # Process video and generate output
+        output_file = infer(video_url, audio_url)
+        thumbnail_url = do_service.generate_thumbnail(video_url, '/tmp', 'user-thumbnail')
+
+        return jsonify({
+            'video_url': video_url,
+            'audio_url': audio_url,
+            'result_url': output_file,
+            'thumbnail_url': thumbnail_url
+        })
+    except Exception as e:
+        abort(500, str(e))
 
 @app.route('/delete/<string:key>', methods=['DELETE'])
 def delete_file(key):
