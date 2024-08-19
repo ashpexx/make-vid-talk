bd6a7bff4c3353aa9068ae20f08685dc8e45d0bb
devsamahd <samahd626@gmail.com>
2024-08-19 20:47:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ async def delete_file(key: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    uvicorn.run(app, host="0.0.0.0", port=47926)
